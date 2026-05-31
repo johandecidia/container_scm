fab65a519b8a72e27eb3fb3d004719b0ac70b319
johandecidia <johan@decidia.se>
2026-05-31 14:35:07 +0200

    INTEGRATIONS | add integration models and migrations

--- apps/scm/integrations/admin.py
+++ apps/scm/integrations/admin.py
@@ -5,6 +5,6 @@ from .models import Integration
 
 @admin.register(Integration)
 class IntegrationAdmin(admin.ModelAdmin):
-    list_display = ["name", "provider", "status", "team", "created_at"]
-    list_filter = ["status", "provider"]
-    search_fields = ["name", "provider"]
+    list_display = ["name", "provider_code", "provider_family", "status", "team", "created_at"]
+    list_filter = ["status", "provider_family", "team"]
+    search_fields = ["name", "provider_code"]
