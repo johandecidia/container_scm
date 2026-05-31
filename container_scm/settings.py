319bd325acd82ca6e35c7a3c0d86af29a404e610
johandecidia <johan@decidia.se>
2026-05-31 16:13:11 +0200

    PRODUCTION READINESS | configure sentry error tracking

--- container_scm/settings.py
+++ container_scm/settings.py
@@ -634,11 +634,14 @@ SENTRY_DSN = env("SENTRY_DSN", default="")
 
 if SENTRY_DSN:
     import sentry_sdk
+    from sentry_sdk.integrations.celery import CeleryIntegration
     from sentry_sdk.integrations.django import DjangoIntegration
 
     sentry_sdk.init(
         dsn=SENTRY_DSN,
-        integrations=[DjangoIntegration()],
+        integrations=[DjangoIntegration(), CeleryIntegration()],
+        environment=env("SENTRY_ENVIRONMENT", default="production"),
+        send_default_pii=False,
     )
 
 LOGGING = {
